8cb2b178cd9796bb03d3dbc85ebfb735b79df62c
David Linner <david@linner.berlin>
2019-10-24 10:16:23 +0200

    Changed webpack build mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    mode:'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'sea-drone.js',
-        library: 'sea-drone',
-        libraryTarget: 'umd'
+        library: 'sea-drone'
+        //libraryTarget: 'umd'
     },
 };
