80f11f64e301e3fa4be85f50066f33030bda2376
Niels Heckmann <26061224+niels-he@users.noreply.github.com>
2019-12-19 14:25:50 +0100

    .

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ const createSimulation = ({
 } = {}) => {
   const runner = Runner.create();
   const world = World.create({ gravity: { x: 0, y: 0, scale: 0 } });
-  const engine = Engine.create({ world, timing: { timeScale: 5 } });
+  const engine = Engine.create({ world, timing: { timeScale: 9 } });
   const render = Render.create({
     element,
     engine,
